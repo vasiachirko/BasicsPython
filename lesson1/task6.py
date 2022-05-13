0b2a42d5d4d71f5402e0235e90e964f816119587
vasiachirko <51925240+vasiachirko@users.noreply.github.com>
2022-05-13 13:01:31 +0300

    deleted old files

--- lesson1/task6.py
+++
@@ -1,8 +0,0 @@
-distanceStart = int(input("Сколько км спортсмен пробежал в первый день"))
-distanceTotal = int(input("Сколько км в день должен пробегать спортсмен"))
-day = 1
-distance = distanceStart
-while distance < distanceTotal:
-    distance *= 1.1
-    day += 1
-print(f"Спортсмен пробежал не менее {distanceTotal} км на {day} день")
