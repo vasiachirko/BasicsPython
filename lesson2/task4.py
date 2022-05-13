142dfe3aa57e440e980825e0516f11a7f463bbc3
vasiachirko <51925240+vasiachirko@users.noreply.github.com>
2022-05-13 13:02:48 +0300

    deleted old files

--- lesson2/task4.py
+++
@@ -1,7 +0,0 @@
-str = input()
-list = str.split(' ')
-for tmp in range(0, list.__len__()):
-    print(f'{tmp}       {list[tmp][:10]}')
-
-for i, word in enumerate(list, start=1):
-    print(f'{i} {word}')
